c693315777d14461bad76e717b9d3e7ccdc258f3
Michał <mlewandowsky@hotmail.com>
2022-10-24 20:12:44 +0200

    Calories counting program read from sheet and add calories of name-chosen product with a gramature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import playground
 
+import_class = playground.Playground()
 print("Welcome to calories counting program!")
 MAX_MEALS = 8
 MIN_MEALS = 1
@@ -14,8 +16,10 @@ for _ in range(0, user_input):
   end = True
   while end:
     print(f"your {_+1} meal")
-    product_input = input("Please add a product?\n").lower()
-    gramature_input = int(input("how much grams?\n").lower())
+    # _____________________________product input musisz dokladnie wpisywac to co jest na liscie.
+    product_input = input(f"Please chose a product to add {import_class.name_chose()}\n")
+    gramature_input = int(input("how much grams of this product ?\n"))
+    print(import_class.data_by_name(product_input, gramature_input))
     finished = str(input("Have you finished on this meal? 'y' for yes or 'n' for no \n")).lower()
     if finished == "y":
       end = False
--- playground.py
+++ playground.py
@@ -1,20 +1,46 @@
-import requests
+from pprint import pprint
+import json
 
 
-# ________________________________________read requests
-SHEET_ENDPOINT = "https://api.sheety.co/39c535bbf835a3ced3a84cfc3061120f/caloriesProgram/arkusz1"
+class Playground:
+    import requests
+    # ________________________________________read requests
+    SHEET_ENDPOINT = "https://api.sheety.co/39c535bbf835a3ced3a84cfc3061120f/caloriesProgram/arkusz1"
 
-response = requests.get(url=SHEET_ENDPOINT)
-data = response.json()
-chicken_cals = data["arkusz1"][0]["testCals"]
-beef_cals = data["arkusz1"][1]["testCals"]
+    response = requests.get(url=SHEET_ENDPOINT)
+    data = response.json()
+    # chicken_cals = data["arkusz1"][0]["testCals"]
+    # beef_cals = data["arkusz1"][1]["testCals"]
+    len_of_names = len(data["arkusz1"])
+    # print(data["arkusz1"][0])
+    # # kurwa jest ___________________________________________________ wyszukiwanie kalorii po nazwie
+    # for _ in range(0, len_of_names):
+    #     if data["arkusz1"][_]["testName"] == "Kurczak":
+    #         print(data["arkusz1"][_]["testCals"])
+    #
 
 
-chicken_name = data["arkusz1"][0]["testName"]
+    def name_chose(self):
+        self.names =[]
+        for name in range(0, self.len_of_names):
+            self.names.append(self.data["arkusz1"][name]["testName"])
+        return self.names
 
-if chicken_name == "Kurczak":
-    print(chicken_cals)
-else : pass
+    def data_by_name(self, prod_input, prod_gram):
+        for _ in range(0, self.len_of_names):
+            if self.data["arkusz1"][_]["testName"] == prod_input:
+                self.cal = float(self.data["arkusz1"][_]["testCals"]) * prod_gram
+                self.protein = float(self.data["arkusz1"][_]["protein"]) * prod_gram
+                self.carbo = float(self.data["arkusz1"][_]["carbo"]) * prod_gram
+                self.fat = float(self.data["arkusz1"][_]["fat"]) * prod_gram
+            return f"cal {self.cal}, prot {self.protein}, carbo {self.carbo}, fat {self.fat}"
 
-# {'arkusz1': [{'testName': 'Kurczak', 'testCals': 1000, 'id': 2}, {'testName': 'Wołowina', 'testCals': 2000, 'id': 3}]}
-# ____________________________________________________
+
+
+
+    # if chicken_name == "Kurczak":
+    #     print(chicken_cals)
+    # else : pass
+
+    # {'arkusz1': [{'testName': 'Kurczak', 'testCals': 1000, 'id': 2}, {'testName': 'Wołowina', 'testCals': 2000, 'id': 3}]}
+    # ____________________________________________________
