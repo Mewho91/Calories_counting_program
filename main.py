b76c4c2e8f705f52bbc5aa75c421e7aea1d2792f
Michał <mlewandowsky@hotmail.com>
2022-10-29 15:30:01 +0200

    Program count and name all added meals by Prot,carb,fat,calories

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
 import playground
 
 fc = playground.Playground()
+# START
 print("Welcome to calories counting program!")
 MAX_MEALS = 8
 MIN_MEALS = 1
 is_ok = True
-while is_ok :
-  user_input = int(input("Select how many meals do you want to count? \n"))
-  if user_input > MAX_MEALS or user_input < MIN_MEALS:
-    print("Are you sure of that ?")
-  else :
-    print(f"You chosed {user_input} meals")
-    is_ok = False
 
+while is_ok:
+
+    user_input = int(input("Select how many meals do you want to count? \n"))
+    if user_input > MAX_MEALS or user_input < MIN_MEALS:
+        print("Are you sure of that ?")
+    else:
+        print(f"You chosed {user_input} meals")
+        is_ok = False
+d ={}
 for meals in range(0, user_input):
+    calories = 0
+    proteins = 0
+    carbo = 0
+    fat = 0
 
-  calories = 0
-  proteins = 0
-  carbo = 0
-  fat = 0
-
-  end = True
-  while end:
-
-    print(f"your {meals + 1} meal")
-    # _____________________________product input musisz dokladnie wpisywac to co jest na liscie.
-    product_input = input(f"Please chose a product to add {fc.name_chose()}\n")
-    gramature_input = int(input("how much grams of this product ?\n"))
-    # print(import_class.data_by_name(product_input, gramature_input))
-    finished = str(input("Have you finished on this meal? 'y' for yes or 'n' for no \n")).lower()
-    calories += fc.count_calories(product_input, gramature_input)
-    proteins += fc.count_protein(product_input, gramature_input)
-    carbo += fc.count_carb(product_input, gramature_input)
-    fat += fc.count_fat(product_input, gramature_input)
-    if finished == "y":
-      print(f"Your {meals + 1} Meal is  calories:  {calories}, carbohydrates : {carbo}, proteins {proteins}, fat {fat}")
-      end = False
-
+    end = True
+    while end:
+        print(f"your {meals + 1} meal")
+        # _____________________________product input musisz dokladnie wpisywac to co jest na liscie.
+        product_input = input(f"Please chose a product to add {fc.name_chose()}\n")
+        gramature_input = int(input("how much grams of this product ?\n"))
+        finished = str(input("Have you finished on this meal? 'y' for yes or 'n' for no \n")).lower()
+        calories += fc.count_calories(product_input, gramature_input)
+        proteins += fc.count_protein(product_input, gramature_input)
+        carbo += fc.count_carb(product_input, gramature_input)
+        fat += fc.count_fat(product_input, gramature_input)
+        if finished == "y":
+            print(f"Your {meals + 1} Meal is  calories:  {calories}, carbohydrates : {carbo}, proteins {proteins}, fat {fat}")
+            end = False
+    d["Meal {0}".format(meals+1)] = f"(Calories : {calories}, proteins : {proteins}, carbohydrates : {carbo}, fat : {fat})"
+    print(d)
--- playground.py
+++ playground.py
@@ -1,9 +1,7 @@
-from pprint import pprint
-import json
+import requests
 
 
 class Playground:
-    import requests
     # ________________________________________read requests
     SHEET_ENDPOINT = "https://api.sheety.co/39c535bbf835a3ced3a84cfc3061120f/caloriesProgram/arkusz1"
 
@@ -19,46 +17,32 @@ class Playground:
     #         print(data["arkusz1"][_]["testCals"])
     #
 
-
     def name_chose(self):
         self.names =[]
         for name in range(0, self.len_of_names):
             self.names.append(self.data["arkusz1"][name]["testName"])
         return self.names
 
-    def data_by_name(self, prod_input, prod_gram):
-        for _ in range(0, self.len_of_names):
-            if self.data["arkusz1"][_]["testName"] == prod_input:
-                self.cal = float(self.data["arkusz1"][_]["testCals"]) * prod_gram
-                self.protein = float(self.data["arkusz1"][_]["protein"]) * prod_gram
-                self.carbo = float(self.data["arkusz1"][_]["carbo"]) * prod_gram
-                self.fat = float(self.data["arkusz1"][_]["fat"]) * prod_gram
-            return f"cal {self.cal}, prot {self.protein}, carbo {self.carbo}, fat {self.fat}"
-
     def count_calories(self, prod_input, prod_gram):
         for _ in range(0, self.len_of_names):
             if self.data["arkusz1"][_]["testName"] == prod_input:
                 self.cal = float(self.data["arkusz1"][_]["testCals"]) * prod_gram
         return self.cal
+
     def count_protein(self, prod_input, prod_gram):
         for _ in range(0, self.len_of_names):
             if self.data["arkusz1"][_]["testName"] == prod_input:
                 self.protein = float(self.data["arkusz1"][_]["protein"]) * prod_gram
         return self.protein
+
     def count_carb(self, prod_input, prod_gram):
         for _ in range(0, self.len_of_names):
             if self.data["arkusz1"][_]["testName"] == prod_input:
                 self.carbo = float(self.data["arkusz1"][_]["carbo"]) * prod_gram
         return self.carbo
+
     def count_fat(self, prod_input, prod_gram):
         for _ in range(0, self.len_of_names):
             if self.data["arkusz1"][_]["testName"] == prod_input:
                 self.fat = float(self.data["arkusz1"][_]["fat"]) * prod_gram
         return self.fat
-
-    # if chicken_name == "Kurczak":
-    #     print(chicken_cals)
-    # else : pass
-
-    # {'arkusz1': [{'testName': 'Kurczak', 'testCals': 1000, 'id': 2}, {'testName': 'Wołowina', 'testCals': 2000, 'id': 3}]}
-    # ____________________________________________________
